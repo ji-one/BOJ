44936cc13ff863af87291126e2423821ef56a617
jiwon kim <rlawldnjs8@gmail.com>
2020-05-13 01:17:48 +0900

    Create 1764.cpp

---
+++ PS/1000~/1764.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<string> v;
+vector<string> rst;
+int main()
+{
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    int n, m;
+    cin >> n >> m;
+    string s;
+    for (int i = 0; i < n + m; i++)
+    {
+        cin >> s;
+        v.push_back(s);
+    }
+    sort(v.begin(), v.end());
+    int cnt = 0;
+    for (int i = 0; i < v.size() - 1; i++)
+    {
+        if (v[i] == v[i + 1])
+        {
+            rst.push_back(v[i]);
+        }
+    }
+    cout << rst.size() << '\n';
+    for (int i = 0; i < rst.size(); i++)
+    {
+        cout << rst[i] << '\n';
+    }
+}
